b9d9210d5d66278d741ee1a11de10a9bdb49c0e5
Sirius <707759383@qq.com>
2021-07-16 16:15:14 +0800

    数据类型

---
+++ javascript/教程/code/20210716.js
@@ -0,0 +1,22 @@
+// 类型
+console.log(typeof 123);
+console.log(typeof '123');
+console.log(typeof false);
+// function
+function f() {}
+console.log(typeof f);
+// undefined
+console.log(typeof undefined);
+console.log(typeof x);
+// object
+console.log(typeof window);
+console.log(typeof {});
+console.log(typeof []);
+
+var o = {};
+var a = [];
+
+console.log(o instanceof Array);
+console.log(a instanceof Array);
+
+console.log(typeof null);
